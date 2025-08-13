2e764054a5dcbfaafb9c77ccabfaaff250e0ef9d
Skiffyyyy <kristiansen.sk68@gmail.com>
2025-08-13 12:47:21 +0200

    Project

---
+++ Script.js
@@ -0,0 +1,13 @@
+function changeDivText(){
+    document.getElementById('myDiv').innerHTML = 'Hello'
+       
+    }
+    function changedivcolor(){
+        document.getElementById('myDiv').classList.add('red')
+    }
+    function changeDivTextback(){
+        document.getElementById('myDiv').innerHTML= 'Empty'
+    }
+    function changedivcolorback(){
+        document.getElementById('myDiv').classList.remove('red')
+    }
